7743dcbcf8bea040ff5bbfaab099b2077f341869
Md Irshad Ansari <irshad@cypherock.com>
2022-03-28 15:59:25 +0530

    Build CI

--- cysync/src/mainProcess/setConfig.ts
+++ cysync/src/mainProcess/setConfig.ts
@@ -1,11 +1,34 @@
 import { app } from 'electron';
 
-import config from '../config';
+import jsonConfig from '../config.json';
 
-process.env.userDataPath = app.getPath('userData');
-process.env.LOG_LEVEL = config.LOG_LEVEL;
-process.env.BUILD_TYPE = config.BUILD_TYPE;
-process.env.SERVER_ENV = config.SERVER_ENV;
+const validateJsonConfig = () => {
+  if (!jsonConfig.GITHUB_REPO) {
+    throw new Error('Invalid GITHUB_REPO in json config');
+  }
+
+  if (!['production', 'debug'].includes(jsonConfig.BUILD_TYPE)) {
+    throw new Error('Invalid BUILD_TYPE in json config');
+  }
+
+  if (
+    !['error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly'].includes(
+      jsonConfig.LOG_LEVEL
+    )
+  ) {
+    throw new Error('Invalid LOG_LEVEL in json config');
+  }
+
+  // This changes between the different servers
+  if (!['production', 'development', 'local'].includes(jsonConfig.SERVER_ENV)) {
+    throw new Error('Invalid SERVER_ENV in json config');
+  }
+
+  // When you want to enable production features on development (`yarn start`)
+  if (typeof jsonConfig.SIMULATE_PRODUCTION !== 'boolean') {
+    throw new Error('Invalid SIMULATE_PRODUCTION in json config');
+  }
+};
 
 /**
  * `IS_PRODUCTION`:
@@ -28,10 +51,22 @@ process.env.SERVER_ENV = config.SERVER_ENV;
  * 1. Use to decide if we want to enable or disable a feature.
  * 2. Example: Websockets, Refresh on Startup etc.
  */
-if (process.env.NODE_ENV === 'production') {
-  process.env.IS_PRODUCTION = 'true';
-} else if (config.simulateProduction) {
-  process.env.IS_PRODUCTION = 'true';
-} else {
-  process.env.IS_PRODUCTION = 'false';
-}
+const setConfig = () => {
+  process.env.userDataPath = app.getPath('userData');
+
+  process.env.LOG_LEVEL = jsonConfig.LOG_LEVEL;
+  process.env.BUILD_TYPE = jsonConfig.BUILD_TYPE;
+  process.env.SERVER_ENV = jsonConfig.SERVER_ENV;
+  process.env.GITHUB_REPO = jsonConfig.GITHUB_REPO;
+
+  if (process.env.NODE_ENV === 'production') {
+    process.env.IS_PRODUCTION = 'true';
+  } else if (jsonConfig.SIMULATE_PRODUCTION) {
+    process.env.IS_PRODUCTION = 'true';
+  } else {
+    process.env.IS_PRODUCTION = 'false';
+  }
+};
+
+validateJsonConfig();
+setConfig();
