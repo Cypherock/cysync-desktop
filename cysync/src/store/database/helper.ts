e3ac7872692e92784af07b3744a88e968395a9f7
HarishTeens <arishh2@gmail.com>
2022-04-01 12:46:16 +0530

    use latestPriceDb in database helper

--- cysync/src/store/database/helper.ts
+++ cysync/src/store/database/helper.ts
@@ -1,20 +1,11 @@
 import { ALLCOINS } from '@cypherock/communication';
 
-import logger from '../../utils/logger';
-
-import { priceDb } from './databaseInit';
+import { latestPriceDb } from './databaseInit';
 
 export const getLatestPriceForCoin = async (coin: string) => {
   if (ALLCOINS[coin] && ALLCOINS[coin].isTest) return 0;
 
-  return priceDb.getPrice(coin.toLowerCase(), 7).then(res => {
-    if (res) {
-      const { length } = res.data;
-      return res.data[length - 1][1];
-    }
-    logger.warn(`Cannot find price for coin ${coin}`);
-    return 0;
-  });
+  return (await latestPriceDb.getPrice(coin)).price;
 };
 
 export const getLatestPriceForCoins = async (coins: string[]) => {
