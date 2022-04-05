fe4d03a75063ecb17bd24e3dedc13084941f2802
Harish <harish@cypherock.com>
2022-04-05 21:02:21 +0530

    improve latestPrice helper

--- cysync/src/store/database/helper.ts
+++ cysync/src/store/database/helper.ts
@@ -1,11 +1,22 @@
 import { ALLCOINS } from '@cypherock/communication';
+import ILatestPrice from '@cypherock/database/dist/models/latestPrice';
+
+import logger from '../../utils/logger';
 
 import { latestPriceDb } from './databaseInit';
 
 export const getLatestPriceForCoin = async (coin: string) => {
   if (ALLCOINS[coin] && ALLCOINS[coin].isTest) return 0;
 
-  return (await latestPriceDb.getPrice(coin)).price;
+  return latestPriceDb
+    .getPrice(coin.toLowerCase())
+    .then((res: ILatestPrice) => {
+      if (res) {
+        return res.price;
+      }
+      logger.warn(`Cannot find price for coin ${coin}`);
+      return 0;
+    });
 };
 
 export const getLatestPriceForCoins = async (coins: string[]) => {
