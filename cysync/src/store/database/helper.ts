9696e746e2e5032cfd55e7bf14cffcbed54b3773
Harish <harish@cypherock.com>
2022-05-02 13:37:54 +0530

    fix latest price not fetched for ERC20 tokens

--- cysync/src/store/database/helper.ts
+++ cysync/src/store/database/helper.ts
@@ -1,5 +1,6 @@
 import { ALLCOINS } from '@cypherock/communication';
 import ILatestPrice from '@cypherock/database/dist/models/latestPrice';
+import { pricing as pricingServer } from '@cypherock/server-wrapper';
 
 import logger from '../../utils/logger';
 
@@ -10,11 +11,21 @@ export const getLatestPriceForCoin = async (coin: string) => {
 
   return latestPriceDb
     .getPrice(coin.toLowerCase())
-    .then((res: ILatestPrice) => {
+    .then(async (res: ILatestPrice) => {
       if (res) {
         return res.price;
       }
-      logger.warn(`Cannot find price for coin ${coin}`);
+      logger.warn(`Couldn't find price for coin ${coin}, trying to fetch it`);
+      const response = await pricingServer.getLatest({
+        coin: coin.toLowerCase()
+      });
+      if (response) {
+        logger.info(`Successfully fetched price for coin ${coin}`);
+        const latestPrice = response.data.data.price;
+        await latestPriceDb.insert(coin.toLowerCase(), latestPrice);
+        return latestPrice;
+      }
+      logger.error(`Cannot fetch ${coin} latest price`);
       return 0;
     });
 };
