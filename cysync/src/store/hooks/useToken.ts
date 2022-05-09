0aaeadeb7d1debfe37cce3de3b1b42c29ab0bbcb
Harish <harish@cypherock.com>
2022-05-09 19:57:08 +0530

    sort token on db change

--- cysync/src/store/hooks/useToken.ts
+++ cysync/src/store/hooks/useToken.ts
@@ -30,6 +30,7 @@ export const useToken: UseToken = () => {
 
   const [currentWalletId, setCurrentWalletId] = useState('');
   const [currentEthCoin, setCurrentEthCoin] = useState('');
+  const [sortIndex, setSortIndex] = useState(0);
 
   // Using doRefresh mechanish because hooks state change do not work with event listeners.
   const [doRefresh, setDoRefresh] = useState(false);
@@ -57,6 +58,10 @@ export const useToken: UseToken = () => {
       erc20tokenDb.emitter.removeListener('delete', onChange);
     };
   }, []);
+
+  useEffect(() => {
+    sortTokenData(tokenData, sortIndex);
+  }, [sortIndex]);
 
   const getTokensWithPrices = async (tokens: ERC20Token[]) => {
     const tokensWithPrice: DisplayToken[] = [];
@@ -180,7 +185,7 @@ export const useToken: UseToken = () => {
 
   const sortTokensByIndex: UseTokenValues['sortTokensByIndex'] = index => {
     if (tokenData.length === 0) return;
-    sortTokenData(tokenData, index);
+    if (index !== sortIndex) setSortIndex(index);
   };
 
   const getAllTokensFromWallet = async (walletId: string, ethCoin: string) => {
@@ -191,7 +196,7 @@ export const useToken: UseToken = () => {
     });
     setTokenList(tokens);
     const unsortedTokens = await getTokensWithPrices(res);
-    sortTokenData(unsortedTokens, 0);
+    sortTokenData(unsortedTokens, sortIndex);
   };
 
   useEffect(() => {
