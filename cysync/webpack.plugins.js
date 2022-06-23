1e14c3c0d56fd814563f7c2defcc0457c70fc68b
Tejasv Sharma <tejasvonly@gmail.com>
2022-06-23 06:40:55 +0530

    fixed hot reload for cysync

--- cysync/webpack.plugins.js
+++ cysync/webpack.plugins.js
@@ -1,7 +1,18 @@
 const webpack = require('webpack');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const relocateLoader = require('@vercel/webpack-asset-relocator-loader');
 
 module.exports = [
   new ForkTsCheckerWebpackPlugin(),
-  new webpack.IgnorePlugin({ resourceRegExp: /osx-temperature-sensor/ })
+  new webpack.IgnorePlugin({ resourceRegExp: /osx-temperature-sensor/ }),
+  {
+    apply(compiler) {
+      compiler.hooks.compilation.tap(
+        'webpack-asset-relocator-loader',
+        compilation => {
+          relocateLoader.initAssetCache(compilation, 'native_modules');
+        }
+      );
+    }
+  }
 ];
