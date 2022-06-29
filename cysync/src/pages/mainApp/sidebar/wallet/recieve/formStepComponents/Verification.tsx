8243c0461576d0bee75e43cf2fe50c5bc5c251d9
Tejasv Sharma <tejasvonly@gmail.com>
2022-06-30 03:18:45 +0530

    changed receive coin verification for custom accounts

--- cysync/src/pages/mainApp/sidebar/wallet/recieve/formStepComponents/Verification.tsx
+++ cysync/src/pages/mainApp/sidebar/wallet/recieve/formStepComponents/Verification.tsx
@@ -1,12 +1,20 @@
+import { COINS, NearCoinData } from '@cypherock/communication';
+import Server from '@cypherock/server-wrapper';
+import wallet, { NearWallet } from '@cypherock/wallet';
+import LaunchIcon from '@mui/icons-material/Launch';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import { shell } from 'electron';
 import React, { useEffect } from 'react';
 
+import CustomIconButton from '../../../../../../designSystem/designComponents/buttons/customIconButton';
 import TextView from '../../../../../../designSystem/designComponents/textComponents/textView';
 import {
+  useCurrentCoin,
   useCustomAccountContext,
   useReceiveTransactionContext
 } from '../../../../../../store/provider';
+import logger from '../../../../../../utils/logger';
 
 import {
   StepComponentProps,
@@ -18,7 +26,8 @@ const PREFIX = 'WalletReceiveVerification';
 const classes = {
   root: `${PREFIX}-root`,
   addressContainer: `${PREFIX}-addressContainer`,
-  copyButton: `${PREFIX}-copyButton`
+  copyButton: `${PREFIX}-copyButton`,
+  transactionId: `${PREFIX}-transactionId`
 };
 
 const Root = styled('div')(() => ({
@@ -44,12 +53,19 @@ const Root = styled('div')(() => ({
   },
   [`& .${classes.copyButton}`]: {
     textTransform: 'none'
+  },
+  [`& .${classes.transactionId}`]: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 }));
 
 const Verification: React.FC<StepComponentProps> = ({ handleNext }) => {
   const { receiveTransaction } = useReceiveTransactionContext();
   const { customAccount } = useCustomAccountContext();
+  const { coinDetails } = useCurrentCoin();
 
   useEffect(() => {
     if (receiveTransaction.verified) {
@@ -58,6 +74,32 @@ const Verification: React.FC<StepComponentProps> = ({ handleNext }) => {
   }, [receiveTransaction.verified]);
 
   const address = receiveTransaction.receiveAddress;
+
+  const handleExternalLink = async () => {
+    const coin = COINS[coinDetails.slug];
+
+    if (!coin) {
+      logger.error('Invalid COIN in coinDetails: ' + coinDetails.slug);
+      return;
+    }
+
+    if (!(coin instanceof NearCoinData)) {
+      logger.error('Non near coin');
+      return;
+    } else {
+      const w = wallet({
+        coinType: coinDetails.slug,
+        xpub: coinDetails.xpub,
+        walletId: coinDetails.walletId
+      });
+      const url = Server.near.wallet.getCreateTxnLink({
+        network: coin.network,
+        address: customAccount.name,
+        publicKey: (w as NearWallet).nearPublicKey
+      });
+      shell.openExternal(url);
+    }
+  };
   return (
     <Root className={classes.root}>
       <div className={classes.addressContainer}>
@@ -65,12 +107,46 @@ const Verification: React.FC<StepComponentProps> = ({ handleNext }) => {
           {customAccount ? customAccount.name : address}
         </Typography>
       </div>
-      <Typography color="textSecondary">Verify Address on Device</Typography>
-      <TextView
-        completed={receiveTransaction.verified}
-        inProgress={!receiveTransaction.verified}
-        text="Please Match the Address on CypherRock X1"
-      />
+      {customAccount ? (
+        <>
+          <div className={classes.transactionId}>
+            <Typography color="textSecondary">Near Explorer</Typography>
+            {/* <Typography
+            style={{ userSelect: 'text' }}
+            color="textPrimary"
+            variant="body1"
+          >
+            Some Text
+          </Typography> */}
+            <CustomIconButton
+              title="Open Link"
+              placement="top-start"
+              onClick={handleExternalLink}
+            >
+              <LaunchIcon fontSize="medium" color="secondary" />
+            </CustomIconButton>
+          </div>
+          <Typography color="textSecondary">
+            Verify Account on Device
+          </Typography>
+          <TextView
+            completed={receiveTransaction.verified}
+            inProgress={!receiveTransaction.verified}
+            text="Please Match the Key from CypherRock X1 and Account Id from desktop with Near Explorer"
+          />
+        </>
+      ) : (
+        <>
+          <Typography color="textSecondary">
+            Verify Address on Device
+          </Typography>
+          <TextView
+            completed={receiveTransaction.verified}
+            inProgress={!receiveTransaction.verified}
+            text="Please Match the Address on CypherRock X1"
+          />
+        </>
+      )}
     </Root>
   );
 };
