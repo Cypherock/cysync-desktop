c992cc9102fdd0c645bd5b0d8e71d2e0f2ef888a
Harish <harish@cypherock.com>
2022-07-12 11:11:42 +0530

    add flow handlers
    for noWalletFound, there was an issue due to redundant code that led to inconsistency

--- cysync/src/store/hooks/flows/useReceiveTransation.ts
+++ cysync/src/store/hooks/flows/useReceiveTransation.ts
@@ -22,6 +22,8 @@ import logger from '../../../utils/logger';
 import { addressDb, receiveAddressDb } from '../../database';
 import { useCurrentCoin, useSelectedWallet, useSocket } from '../../provider';
 
+import * as flowHandlers from './handlers';
+
 export interface HandleReceiveTransactionOptions {
   connection: DeviceConnection;
   sdkVersion: string;
@@ -186,18 +188,7 @@ export const useReceiveTransaction: UseReceiveTransaction = () => {
       });
 
       receiveTransaction.on('noWalletFound', (walletState: WalletStates) => {
-        const cyError = new CyError();
-        switch (walletState) {
-          case WalletStates.NO_WALLET_FOUND:
-            cyError.setError(CysyncError.NO_WALLET_ON_DEVICE);
-            break;
-          case WalletStates.WALLET_NOT_PRESENT:
-            cyError.setError(CysyncError.WALLET_NOT_FOUND_IN_DEVICE);
-            break;
-          case WalletStates.WALLET_PARTIAL_STATE:
-            cyError.setError(CysyncError.WALLET_PARTIAL_STATE);
-            break;
-        }
+        const cyError = flowHandlers.noWalletFound(walletState);
         setErrorObj(
           handleErrors(errorObj, cyError, flowName, {
             coinType,
