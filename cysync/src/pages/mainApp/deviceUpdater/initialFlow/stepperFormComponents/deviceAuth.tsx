c4a56657d6a945a5b2c29bc1b58d1b92dda9fdb3
Harish <harish@cypherock.com>
2022-08-08 16:05:40 +0530

    PRD-1052 Fix spinner going crazy on device auth
    when internet gets disconnected, both the spinners spin

--- cysync/src/pages/mainApp/deviceUpdater/initialFlow/stepperFormComponents/deviceAuth.tsx
+++ cysync/src/pages/mainApp/deviceUpdater/initialFlow/stepperFormComponents/deviceAuth.tsx
@@ -81,8 +81,7 @@ const DeviceAuthentication: React.FC<StepComponentProps> = ({
     deviceState,
     setDeviceSerial,
     deviceConnectionState,
-    setIsInFlow,
-    connected
+    setIsInFlow
   } = useConnection();
   const latestDeviceConnection = useRef<any>();
 
@@ -215,7 +214,10 @@ const DeviceAuthentication: React.FC<StepComponentProps> = ({
         >
           Follow the Steps on Device
         </Typography>
-        <DynamicTextView text="Connect X1 wallet" state={connected ? 2 : 1} />
+        <DynamicTextView
+          text="Connect X1 wallet"
+          state={latestDeviceConnection.current ? 2 : 1}
+        />
         <br />
         <DynamicTextView
           text="Authenticating Device"
