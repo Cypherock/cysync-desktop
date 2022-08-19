f325086281c70eede32c01dbc78b5d0bcf4c57aa
Harish <harish@cypherock.com>
2022-08-19 14:24:06 +0530

    CSI-1125 Pick only Cysync window as input source

--- cysync/src/utils/recorder.ts
+++ cysync/src/utils/recorder.ts
@@ -5,7 +5,9 @@ export async function initRecorder() {
   const inputSources = await desktopCapturer.getSources({
     types: ['window']
   });
-  const cySyncSource = inputSources[0];
+  const cySyncSource = inputSources.find(src => src.name === 'cySync');
+  // A safety check to ensure we are reporting if we cySync window source is not visible or not accessible.
+  if (!cySyncSource) throw new Error('Cannot find Cysync source');
   const constraints = {
     audio: false,
     video: {
