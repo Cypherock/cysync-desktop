5dc172fe4f641518d9b7eaea09582377a1f243f2
irshadCypherock <irshad@cypherock.com>
2022-09-10 12:30:26 +0530

    Improved error messages

--- cysync/src/constants/i18n/en.ts
+++ cysync/src/constants/i18n/en.ts
@@ -11,7 +11,7 @@ const en: I18nStrings = {
       `Some unknown communication error while ${flow}`,
 
     NETWORK_FAILURE:
-      'Some internal error occurred while communicating with the server, Try again.',
+      'Some internal error occurred while communicating with the server, retry.',
     NETWORK_UNREACHABLE:
       'Failed to communicate with the server, check your internet connection and try again.',
 
@@ -19,18 +19,18 @@ const en: I18nStrings = {
       'Connect the X1 wallet before proceeding with this operation',
     DEVICE_DISCONNECTED_IN_FLOW: 'X1 wallet disconnected, reconnect',
     DEVICE_TIMEOUT_ERROR:
-      'Some internal error occurred\n, this maybe due to inactivity on the X1 wallet',
+      'Some internal error occurred\n This maybe due to inactivity on the X1 wallet',
     DEVICE_NOT_READY:
-      'Bring the X1 wallet to the main menu before starting any operation.',
+      'Bring the X1 wallet to the main menu before starting any operation',
     DEVICE_NOT_READY_IN_INITIAL:
       'Error connecting the X1 wallet, reconnect and try again.',
     DEVICE_NOT_SUPPORTED:
       'The connected X1 wallet is not supported, make sure that CySync and X1 wallet are up to date.',
-    // I dont understand how this message would be helpful.
+    // I dont understand how this message would be helpful
     DEVICE_MISCONFIGURED:
       'Your X1 wallet is unauthenticated, update it to the latest firmware. If the problem persists, contact us.',
-    DEVICE_INFO_UNKNOWN_ERROR: `Some internal error occurred, disconnect and reconnect the X1 wallet.`,
-    DEVICE_NOT_IN_UPDATE_RECEIVING_MODE: `Some internal error occurred, disconnect and reconnect the X1 wallet.`,
+    DEVICE_INFO_UNKNOWN_ERROR: `Some internal error occurred, disconnect and reconnect the X1 wallet then retry.`,
+    DEVICE_NOT_IN_UPDATE_RECEIVING_MODE: `Some internal error occurred, disconnect and reconnect the X1 wallet then retry.`,
 
     WALLET_NOT_FOUND_IN_DEVICE:
       'This wallet account does not seem to be present on the X1 wallet',
@@ -43,9 +43,9 @@ const en: I18nStrings = {
     NO_WALLET_ON_DEVICE:
       'No wallet accounts found, create a new wallet account on the X1 wallet first.',
     WALLET_IS_LOCKED:
-      'Wallet account is locked on the X1 wallet\n, unlock it before proceeding with any operations on the wallet account.',
+      'Wallet account is locked on the X1 wallet\n Unlock it before proceeding with any operations on the wallet account',
     WALLET_LOCKED_DUE_TO_INCORRECT_PIN:
-      'Your wallet account is now locked due to multiple incorrect attempts\n, unlock it before proceeding with any operations on the wallet account',
+      'Your wallet account is now locked due to multiple incorrect attempts\n Unlock it before proceeding with any operations on the wallet account',
     WALLET_UNKNOWN_STATE:
       'Unknown wallet account state received from X1 wallet',
 
@@ -58,7 +58,7 @@ const en: I18nStrings = {
       'This wallet account already exists on the CySync App with different configuration, do you want to update the wallet account details?',
     ADD_WALLET_WITH_SAME_NAME:
       'A wallet account with the same name already exists on the CySync App',
-    ADD_WALLET_UNKNOWN_ERROR: `Some internal error occurred, disconnect and reconnect the X1 wallet then try adding a wallet account`,
+    ADD_WALLET_UNKNOWN_ERROR: `Some internal error occurred, disconnect and reconnect the X1 wallet then retry`,
 
     ADD_COIN_FAILED_DUE_TO_SERVER_ERROR: coins =>
       `Failed to communicate with the blockchain while adding ${coins}, check your internet connection.`,
@@ -66,16 +66,15 @@ const en: I18nStrings = {
       `Some internal error occurred while adding ${coins}`,
     ADD_COIN_FAILED: coins => `Error while adding coins: ${coins}`,
     ADD_COIN_REJECTED:
-      'Request was rejected on the X1 wallet\n, restart the operation to add coin(s).',
-    ADD_COIN_UNKNOWN_ERROR: `Some internal error occurred, disconnect and reconnect the X1 wallet then try adding coins.`,
+      'Request was rejected on the X1 wallet\n Restart the operation to add coin(s)',
+    ADD_COIN_UNKNOWN_ERROR: `Some internal error occurred, disconnect and reconnect the X1 wallet then retry.`,
 
     CARD_AUTH_REJECTED: 'X1 Card auth was rejected from X1 wallet',
     CARD_AUTH_FAILED:
       'X1 Card authentication failed, contact Cypherock immediately.',
     CARD_AUTH_DEVICE_PAIRING_FAILED: 'Device-Card pairing failed',
     UNKNOWN_CARD_ERROR: 'Unknown X1 card error occurred',
-    CARD_AUTH_UNKNOWN_ERROR:
-      'Some internal error occurred, Retry the operation.',
+    CARD_AUTH_UNKNOWN_ERROR: 'Some internal error occurred, retry.',
 
     DEVICE_AUTH_REJECTED: 'Device auth was rejected from the X1 wallet',
     DEVICE_AUTH_FAILED:
@@ -90,7 +89,7 @@ const en: I18nStrings = {
       'Error connecting to the X1 wallet, reconnect the X1 wallet and try again.',
     DEVICE_UPGRADE_KNOWN_ERROR:
       'An internal error occurred, report it to Cypherock immediately.',
-    DEVICE_UPGRADE_UNKNOWN_ERROR: `Some internal error occurred, disconnect and reconnect the X1 wallet then retry updating the X1 wallet.`,
+    DEVICE_UPGRADE_UNKNOWN_ERROR: `Some internal error occurred, disconnect and reconnect the X1 wallet then retry.`,
 
     LOG_FETCHER_REJECTED:
       'Request rejected from X1 wallet, try attaching the X1 wallet logs again.',
@@ -98,11 +97,10 @@ const en: I18nStrings = {
       'Logs are disabled on the X1 wallet, enable it from X1 wallet settings and try again.',
     LOG_FETCHING_CANCEL_FAILED:
       'Something went wrong while canceling logs fetching',
-    LOG_FETCHER_UNKNOWN_ERROR:
-      'Some internal error occurred, reattach the X1 wallet logs.',
+    LOG_FETCHER_UNKNOWN_ERROR: 'Some internal error occurred, retry.',
 
     RECEIVE_TXN_REJECTED: coin =>
-      `Request was rejected on the X1 wallet\n, restart the operation to receive ${coin} on the CySync app.`,
+      `Request was rejected on the X1 wallet\n Restart the operation to receive ${coin} on the CySync app`,
     RECEIVE_TXN_DEVICE_MISCONFIGURED:
       'Looks like your X1 wallet has become inaccessible, do you want to fix it now?',
     RECEIVE_TXN_GENERATE_UNVERIFIED_FAILED:
@@ -112,7 +110,7 @@ const en: I18nStrings = {
     RECEIVE_TXN_DIFFERENT_ADDRESS_BY_USER: `Contact cypherock if the addresses on the Cysync app did not match the addresses on the X1 wallet`,
 
     SEND_TXN_REJECTED: (coin: string) =>
-      `Request was rejected on the X1 wallet\n, restart the operation to send ${coin} on the CySync app.`,
+      `Request was rejected on the X1 wallet\n Restart the operation to send ${coin} on the CySync app`,
     SEND_TXN_INSUFFICIENT_BALANCE: (coin: string) =>
       `You do not have enough ${coin} to make this transaction`,
     SEND_TXN_BLOCKED_UTXOS_WITH_SUFFICIENT_BALANCE: `This transaction requires UTXOs from your previous transaction, wait for few minutes before retrying.`,
