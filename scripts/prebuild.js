a90ac799111bd602a1b9f3b81610fe8ce5a4ed77
irshadCypherock <irshad@cypherock.com>
2022-09-12 13:06:58 +0530

    ci: ci build package version fix

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,9 +1,16 @@
+const axios = require("axios");
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
 const childProcess = require("child_process");
 
 const BRANCH_OR_TAG_NAME = process.env.GITHUB_REF_NAME;
+const GITHUB_BASE_API = "https://api.github.com";
+const GITHUB_ACCESS_TOKEN = process.env.GH_ACCESS_TOKEN;
+
+const RELEASE_FILE_NAME = "RELEASES.txt";
+
+console.log({ BRANCH_OR_TAG_NAME });
 
 const BUILD_TYPE_CONFIG = {
   dev: {
@@ -88,6 +95,116 @@ const getCommitHash = () => {
   });
 };
 
+// Gets the previos version and determines the next index
+const getReleaseIndex = async ({ githubRepo }) => {
+  let previousFileContent = "";
+
+  const packageJsonPath = path.join(__dirname, "..", "cysync", "package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+
+  try {
+    const fileContentResp = await axios.get(
+      `${GITHUB_BASE_API}/repos/${githubRepo}/contents/${RELEASE_FILE_NAME}`,
+      { headers: { Authorization: `token ${GITHUB_ACCESS_TOKEN}` } }
+    );
+
+    if (!fileContentResp?.data?.content) {
+      throw new Error("Cannot find file content");
+    }
+    previousFileContent = Buffer.from(fileContentResp.data.content, "base64")
+      .toString()
+      .trim();
+    sha = fileContentResp.data.sha;
+  } catch (error) {
+    if (error?.response?.status !== 404) {
+      throw error;
+    }
+  }
+
+  let currentIndex = 1;
+
+  if (previousFileContent) {
+    const contentArr = previousFileContent.split("\n");
+    if (contentArr.length > 0) {
+      const version = contentArr[0];
+      const versionArr = version.split("-");
+      if (versionArr.length !== 2) {
+        throw new Error("Invalid version " + version);
+      }
+
+      const baseVersion = versionArr[0];
+      if (versionArr[1].split(".").length !== 2) {
+        throw new Error("Invalid version " + version);
+      }
+
+      if (baseVersion === packageJson.version) {
+        const index = versionArr[1].split(".")[1].trim();
+        if (!index || isNaN(index)) {
+          throw new Error("Invalid version " + version);
+        }
+
+        currentIndex = parseInt(index, 10) + 1;
+      }
+    }
+  }
+
+  return currentIndex;
+};
+
+const setVersion = async (buildType, tagName) => {
+  const packageJsonPath = path.join(__dirname, "..", "cysync", "package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+
+  const version = packageJson.version;
+  if (!version && version.split(".").length !== 3) {
+    throw new Error("Invalid version in package json");
+  }
+
+  const versionArr = version.split(".");
+  if (versionArr.length < 3) {
+    throw new Error("Invalid version in package json");
+  }
+
+  const usableVersionArr = [];
+  for (const v of versionArr) {
+    if (v.includes("-") || Number.isNaN(v)) {
+      throw new Error("Invalid version in package json");
+    }
+
+    usableVersionArr.push(v);
+    if (usableVersionArr.length === 3) break;
+  }
+
+  const usableVersion = usableVersionArr.join(".");
+
+  const index = ["dev", "debug"].includes(buildType)
+    ? await getReleaseIndex({
+        githubRepo: BUILD_TYPE_CONFIG[buildType].GITHUB_REPO,
+      })
+    : 1;
+
+  switch (buildType) {
+    case "dev":
+      packageJson.version = `${usableVersion}-dev.${index}`;
+      break;
+    case "debug":
+      packageJson.version = `${usableVersion}-debug.${index}`;
+      break;
+    case "rc":
+      let newName = tagName;
+      if (newName.startsWith("v")) {
+        newName = newName.slice(1);
+      }
+      packageJson.version = newName;
+      break;
+    case "prod":
+    default:
+      packageJson.version = usableVersion;
+  }
+
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, undefined, 2));
+};
+
 const setDependencies = () => {
   if (process.platform === "darwin") {
     const packageJsonPath = path.join(
@@ -117,9 +234,11 @@ const setDependencies = () => {
 
 const run = async () => {
   try {
-    const { buildType } = getArgs();
+    const { buildType, tagName } = getArgs();
+    console.log({ buildType });
     await setConfig(buildType);
     setDependencies();
+    await setVersion(buildType, tagName);
   } catch (error) {
     console.error(error);
     process.exit(1);
