971cf633858aba8d0aabc2a1b7a2207cbc2f9482
Tejasv Sharma <tejasvonly@gmail.com>
2023-02-15 12:57:35 +0530

    fix: use default gaslimit for arbitrum

--- cysync/src/pages/mainApp/sidebar/wallet/send/stepperForm.tsx
+++ cysync/src/pages/mainApp/sidebar/wallet/send/stepperForm.tsx
@@ -335,11 +335,12 @@ const SendForm: React.FC<StepperProps> = ({
   const triggerCalcGasLimit = async () => {
     const coin = COINS[coinDetails.coinId];
 
+    let defaultLimit = 21000;
+    // Refer: https://api.arbiscan.io/api?module=proxy&action=eth_estimateGas&data=0x&to=0xf6c3c3621f42ec1f1cd1207bb1571d93646ab29a&value=0xff22&gasPrice=0x51da038cc&gas=0x5f5e0ff&apikey=YourApiKeyToken
+    if (coin.id === EthCoinMap.arbitrum) defaultLimit = 255595;
+
     if (!token && !txnParams?.data) {
-      let limit = 21000;
-      // Refer: https://api.arbiscan.io/api?module=proxy&action=eth_estimateGas&data=0x&to=0xf6c3c3621f42ec1f1cd1207bb1571d93646ab29a&value=0xff22&gasPrice=0x51da038cc&gas=0x5f5e0ff&apikey=YourApiKeyToken
-      if (coin.id === EthCoinMap.arbitrum) limit = 255595;
-      setGasLimit(limit);
+      setGasLimit(defaultLimit);
       return;
     }
 
@@ -395,7 +396,8 @@ const SendForm: React.FC<StepperProps> = ({
         amount,
         contractAddress,
         txnParams?.data
-      )) ?? (txnParams?.gas ? parseInt(txnParams?.gas, 16) : 2100);
+      )) ??
+      (parseInt(txnParams?.gas, 16) || defaultLimit);
 
     if (estimatedLimit) {
       setGasLimit(estimatedLimit);
